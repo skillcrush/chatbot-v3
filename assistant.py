64d772f46ae9249fca8278112cd186e21a2dbc10
lsavoie <twosavoie@gmail.com>
2024-04-16 01:07:41 -0500

    Added code to upload files to assistant

--- assistant.py
+++ assistant.py
@@ -10,71 +10,79 @@ logging.basicConfig(filename = "assistant.log", level = logging.INFO)
 
 client = OpenAI()
 
-def process_run(thread_id, assistant_id):
-    new_run = client.beta.threads.runs.create(
-        thread_id = thread_id,
-        assistant_id = assistant_id
-    )
-
-    phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
-
-    while True:
-        time.sleep(1)
-        print(random.choice(phrases) + "...")
-        run_check = client.beta.threads.runs.retrieve(
-            thread_id = thread_id,
-            run_id = new_run.id
-        )
-        if run_check.status in ["cancelled", "failed", "completed", "expired"]:
-            return run_check
-
-def log_run(run_status):
-    if run_status in ["cancelled", "failed", "expired"]:
-        log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
-
-def get_message(run_status):
-    if run_status == "completed":
-        thread_messages = client.beta.threads.messages.list(
-            thread_id = thread.id
-        )
-        message = thread_messages.data[0].content[0].text.value
-
-    if run_status in ["cancelled", "failed", "expired"]:
-        message = "An error has occurred, please try again."
-    
-    return message
-
-assistant = client.beta.assistants.create(
-    name = "Study Buddy",
-    model = "gpt-3.5-turbo",
-    instructions = "You are a study partner for students who are newer to technology. When you answer prompts, do so with simple language suitable for someone learning fundamental concepts.",
-    tools=[]
+# After uploading the file to the OpenAI platform, this block of code and print statement can be removed and the code below can be uncommented.
+curriculum_knowledge = client.files.create(
+    file = open("knowledge/OpenAIChatCompletionsAPICheatsheet.pdf", "rb"),
+    purpose = "assistants"
 )
 
-thread = client.beta.threads.create()
+print(curriculum_knowledge)
 
-user_input = ""
+# def process_run(thread_id, assistant_id):
+#     new_run = client.beta.threads.runs.create(
+#         thread_id = thread_id,
+#         assistant_id = assistant_id
+#     )
 
-while True:
-    if (user_input == ""):
-        user_input = input("Assistant: Hello there! Just so you know, you can type exit to end our chat. What's your name? ")
-    else:
-        user_input = input("You: ")
+#     phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
 
-    if user_input.lower() == "exit":
-        print("Goodbye!")
-        break
+#     while True:
+#         time.sleep(1)
+#         print(random.choice(phrases) + "...")
+#         run_check = client.beta.threads.runs.retrieve(
+#             thread_id = thread_id,
+#             run_id = new_run.id
+#         )
+#         if run_check.status in ["cancelled", "failed", "completed", "expired"]:
+#             return run_check
 
-    message = client.beta.threads.messages.create(
-        thread_id = thread.id,
-        role = "user",
-        content = user_input
-    )
+# def log_run(run_status):
+#     if run_status in ["cancelled", "failed", "expired"]:
+#         log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
 
-    run = process_run(thread.id, assistant.id)
+# def get_message(run_status):
+#     if run_status == "completed":
+#         thread_messages = client.beta.threads.messages.list(
+#             thread_id = thread.id
+#         )
+#         message = thread_messages.data[0].content[0].text.value
 
-    log_run(run.status)
+#     if run_status in ["cancelled", "failed", "expired"]:
+#         message = "An error has occurred, please try again."
+    
+#     return message
 
-    message = get_message(run.status)
+# assistant = client.beta.assistants.create(
+#     name = "Study Buddy",
+#     model = "gpt-3.5-turbo",
+#     instructions = "You are a study partner for students who are newer to technology. When you answer prompts, do so with simple language suitable for someone learning fundamental concepts.",
+#     tools=[]
+# )
 
-    print("\nAssistant: " + message + "\n")
+# thread = client.beta.threads.create()
+
+# user_input = ""
+
+# while True:
+#     if (user_input == ""):
+#         user_input = input("Assistant: Hello there! Just so you know, you can type exit to end our chat. What's your name? ")
+#     else:
+#         user_input = input("You: ")
+
+#     if user_input.lower() == "exit":
+#         print("Goodbye!")
+#         break
+
+#     message = client.beta.threads.messages.create(
+#         thread_id = thread.id,
+#         role = "user",
+#         content = user_input
+#     )
+
+#     run = process_run(thread.id, assistant.id)
+
+#     log_run(run.status)
+
+#     message = get_message(run.status)
+
+#     print("\nAssistant: " + message + "\n")
